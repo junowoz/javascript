8fce54c2c07697139326303a24653b6914c01daf
junowoz <junogouvea@gmail.com>
2022-06-30 18:07:26 -0400

    js scrimba 29 jun

--- exercices/scrimba_practice.js
+++ exercices/scrimba_practice.js
@@ -133,24 +133,25 @@ if (listIngredients.includes("chocolate")) {  //if true, then
 
 //////////////////////////////////////////////////////////////////////////////
 //Import & Export example.js
+/**
 export const data = [1, 2, 3]; //here I export
 
-    //index.js -- another file
-    import { data } from './example.js';
+//index.js -- another file
+import { data } from './example.js';
         
     updatedData.push(5)
     console.log(updatedData);
 
-/**
+
  * Challenge 
  * Inside of the file data.js, create a function add, that will receive 2 numbers and return the sum of them.
  * Make sure to export this function.
 - Import the function add, into the index.js file
 - Create a variable result, that will hold the result of the function add when you call it and pass 2 numbers into it.
 - print into the console the value of the variable result;
- */
+ 
 
-// data.js
+//data.js
 export function add(x, y) {
     return x + y;
 }
@@ -159,6 +160,64 @@ export function add(x, y) {
 import { add } from "./data.js";
 let result = add(3, 2);
 console.log(result);
+*/
 
 //////////////////////////////////////////////////////////////////////////////
 //padStart() & padEnd()
+let example = 'Dylan';
+console.log(example.padEnd(10, 'a'));
+//Dylanaaaaaaaa
+
+let exa = 'Dylan';
+console.log(exa.padStart(10, 'a'));
+//aaaaaDylan
+
+
+//////////////////////////////////////////////////////////////////////////////
+/**
+ *Classes Challenge**:
+
+Create a class Player with the following:
+- Add a Name and Country properties
+- Add a function that when it runs should print into the console ("Messi was born in Argentina");
+- Make sure to adapt this function to receive dynamic Names and Clubs.
+
+Create a Subclass called TennisPlayer that extends from the class Player
+- Add a new property Age.
+- Add a function that when it runs should print into the console something similar ("Rafael Nadal is 34 years old and knows how to play Tennis");
+- Make sure the Name and Age are dynamic.
+ */
+
+class Player {
+    constructor(name, country) {
+        this.name = name;
+        this.country = country;
+    }
+    
+    introduce() {
+        console.log(`${this.name} was born in ${this.country}`);
+    }
+}
+
+const messi = new Player("Messi", "Argentina");
+
+messi.introduce();
+
+class TennisPlayer extends Player {
+    constructor(name, country, age) {
+        super(name, country);
+        this.age = age;
+    }
+    
+    playTennis() {
+        console.log(`${this.name} is ${this.age} years old and knows how to play Tennis`);
+    }
+}
+
+const roger = new TennisPlayer( "Roger Federer", "Spain", 38);
+
+roger.introduce();
+roger.playTennis();
+
+//////////////////////////////////////////////////////////////////////////////
+//Trailing Commas
